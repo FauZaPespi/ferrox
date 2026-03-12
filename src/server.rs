b10677d1a6ef55e38712caaa63d16066853c6796
NeoTech <ptysiamaksim@gmail.com>
2026-03-12 15:59:07 +0100

    fix: Prevented slowloris and DoS attacks by implementing time-outs, better concurrency and request limitation. Ferrox is now more suitable for production.

--- src/server.rs
+++ src/server.rs
@@ -1,36 +1,55 @@
 use std::io::{Read, Write};
 use std::net::{IpAddr, TcpListener, TcpStream};
-use std::{thread};
+use std::time::Duration;
 use time::UtcDateTime;
+use threadpool::ThreadPool;
 
 use crate::handlers::static_files::serve_file;
 use crate::http::request::Request;
 use crate::http::response::{Body, Response};
 
-pub fn serve(addr: &str) {
-    let listener: TcpListener = TcpListener::bind(addr).unwrap();
+const MAX_HEADER_SIZE: u64 = 8192; // 8KB
+const MAX_WORKERS: usize = 4;
+const READ_TIMEOUT_SEC: u64 = 5;
+const WRITE_TIMEOUT_SEC: u64 = 5;
 
-    println!("Ferrox running on http://{}", addr);
+pub fn serve(addr: &str) {
+    let listener = TcpListener::bind(addr).unwrap();
+    let pool = ThreadPool::new(MAX_WORKERS);
+
+    println!("Ferrox running on http://{addr} with {MAX_WORKERS} workers");
 
     for stream in listener.incoming() {
-        let stream: TcpStream = stream.unwrap();
-        thread::spawn(|| {
-            if let Err(e) = handle(stream) {
-                eprintln!("Connection error: {}", e);
+        match stream {
+            Ok(stream) => {
+                let _ = stream.set_read_timeout(Some(Duration::from_secs(READ_TIMEOUT_SEC)));
+                let _ = stream.set_write_timeout(Some(Duration::from_secs(WRITE_TIMEOUT_SEC)));
+
+                pool.execute(move || {
+                    if let Err(e) = handle(stream) {
+                        eprintln!("Connection error: {}", e);
+                    }
+                });
             }
-        });
+            Err(e) => eprintln!("Failed to accept connection: {}", e),
+        }
     }
 }
 
 fn handle(mut stream: TcpStream) -> std::io::Result<()> {
-    let mut buffer: [u8; 1024] = [0; 1024];
-    let size: usize = stream.read(&mut buffer).unwrap();
+    let mut buffer: [u8; 8192] = [0; MAX_HEADER_SIZE as usize];
+
+    let bytes_read = stream.read(&mut buffer)?;
+
+    if bytes_read == 0 {
+        return Ok(());
+    }
 
     let connecting_ip: IpAddr = stream.peer_addr()?.ip();
     let requested_ip: IpAddr = stream.local_addr()?.ip();
     let date: UtcDateTime = UtcDateTime::now();
 
-    let request: Request = Request::parse(&buffer[..size]);
+    let request: Request = Request::parse(&buffer[..bytes_read]);
 
     let mut response: Response = match serve_file(&request.path) {
         Ok(r) => r,
